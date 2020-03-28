5eccecb54b698a0bedf017de512f1a23eb7b44fd
Glitch (hello-express) <none>
2020-03-28 11:54:33 +0000

    🎤🎇 Checkpoint ./package.json:451153/21 ./server.js:451153/398

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server( { port: 8080 } );
+wss.on('connection', function connection(ws) {
+
+  ws.on('message', function ( message ) {
+
+    console.log('received: %s', message );
+
+  } );
+ 
+  ws.send( 'something' );
+
+} );
+
 // server.js
 // where your node app starts
 
