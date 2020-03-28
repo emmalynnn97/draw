4f587faaff0552202c84275097a4757e59785c18
Glitch (hello-express) <none>
2020-03-28 12:42:38 +0000

    📚⏱ Checkpoint ./views/index.html copy:451153/1604 ./views/index.html:451153/2634 ./server.js:451153/1640

--- server.js
+++ server.js
@@ -5,95 +5,16 @@ const expressWs = require( 'express-ws' )( app );
 
 app.use( express.static( 'public' ) );
 app.get( '/', function ( request, response ) {
-
   response.sendFile( __dirname + '/views/index.html' );
-
 } );
 
 app.ws( '/', function ( ws, request ) {
-  
   ws.on( 'message', function( message ) {
-
     console.log( message );
-
   } );
-
-  console.log( 'socket', request.testing );
-
+  console.log( 'socket' );
 } );
 
 const listener = app.listen( process.env.PORT, function () {
-
   console.log( "Your app is listening on port " + listener.address().port );
-
 } );
-
-/*
-const WebSocket = require( 'ws' );
-const wss = new WebSocket.Server( { port: 8080 } );
-
-wss.on( 'connection', function ( ws ) {
-
-  ws.on( 'message', function ( message ) {
-
-    console.log('received: %s', message );
-
-  } );
- 
-  ws.send( 'something' );
-
-} );
-
-//
-
-// const fs = require( 'fs' );
-const http = require('http');
-
-function handleRequest( request, response ) {
-
-  response.writeHead( 200, { 'Content-Type': 'text/html' } );
-  response.write( 'Hello World!' ); 
-  response.end();
-
-}
-
-const server = http.createServer( handleRequest ).listen( process.env.PORT );
-*/
-
-
-/*
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-*/
