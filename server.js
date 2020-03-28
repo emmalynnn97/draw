e9650b432a133457421bf5063ec7eccd28dbeb0c
Glitch (hello-express) <none>
2020-03-28 12:30:32 +0000

    🌮💂 Checkpoint ./server.js:451153/761

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-var express = require( 'express' );
-var expressWs = require( 'express-ws' );
+const express = require( 'express' );
+const app = express();
+
+const expressWs = require( 'express-ws' )( app );
+
+app.use( express.static( 'public' ) );
+app.get( '/', function ( request, response ) {
+
+  response.sendFile( __dirname + '/views/index.html' );
+
+} );
+
+app.ws( '/', function ( ws, request ) {
+  
+  ws.on( 'message', function( message ) {
+
+    console.log( message );
+
+  } );
+
+  console.log( 'socket', request.testing );
+
+} );
+
+const listener = app.listen( process.env.PORT, function () {
+
+  console.log( "Your app is listening on port " + listener.address().port );
+
+} );
 
 /*
 const WebSocket = require( 'ws' );
