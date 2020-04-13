6ec42c471202d012657d94cfd7ca23c2c1e38d07
Glitch (multiuser-sketchpad) <none>
2020-04-13 13:02:59 +0000

    😎🎽 Checkpoint ./public/js/Player.js:451153/2307 ./public/index.html:451153/887

--- public/js/Player.js
+++ public/js/Player.js
@@ -3,39 +3,62 @@ function Player( context ) {
   let cx = 0;
   let cy = 0;
   
-  let byteOffset = 0;
+  let byteTotal = ( 2 * 4 ) + ( 30 * 2 );
+  let byteOffset = byteTotal;
+  
   let data;
 
   return {
     setBuffer: function ( buffer ) {
       data = new DataView( buffer );
+      byteOffset = 0;
+    },
+    draw: function ( x1, y1, x2, y2 ) {
 
       context.beginPath();
-
-      let cx = data.getUint16( 0 );
-      let cy = data.getUint16( 2 );
-
-      context.moveTo( cx, cy );
-
-      for ( var i = 0; i <= 60; i += 2 ) {
-        context.lineTo( cx, cy );
-
-      }
-
+      context.moveTo( x1, y1 );
+      context.lineTo( x2, y2 );
       context.strokeStyle = 'blue';
       context.stroke();
       
     },
     next: function () {
       
+      if ( byteOffset === byteTotal ) return;
+
+      let mx = 90, my;
+
       if ( byteOffset === 0 ) {
+        
+        if ( cx > 0 ) {
+          
+          mx = cx;
+          my = cy;
+          
+        }
+
         cx = data.getUint16( 0 );
         cy = data.getUint16( 2 );
         byteOffset += 4;
-      } else {
-        cx += data.getInt8( 4 + i + 0 );
-        cy += data.getInt8( 4 + i + 1 );        
+        
+        if ( mx ) {
+          
+          this.draw( mx, my, cx, cy );          
+          
+        }
+        
+        isLine = true;
+
       }
+      
+      mx = cx;
+      my = cy;
+
+      cx += data.getInt8( byteOffset + 0 );
+      cy += data.getInt8( byteOffset + 1 );
+      byteOffset += 2;
+
+      this.draw( mx, my, cx, cy );          
       
     }
     
